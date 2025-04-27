eaf025dc276c25137b7da62dd2facece92a0c4ff
Luis <lsuizo72@gmail.com>
2025-04-27 14:50:16 +0800

    Add languge route for testing

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\LanguageController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', [AuthController::class, 'authenticate']);
     Route::post('/logout', [AuthController::class, 'unauthenticate'])->middleware('auth:sanctum');
 });
+
+Route::apiResource('languages', LanguageController::class);
